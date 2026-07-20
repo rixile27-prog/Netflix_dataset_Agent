43b60c9f7993bfbe8370c7c06a1653ab3e2eff25
rixile27@gmail.com <rixile27@gmail.com>
2026-07-20 19:12:54 +0000

    uploading netflix code
    uploading netflix code

--- Netflix_dataset_agentt.sql
+++ Netflix_dataset_agentt.sql
@@ -90,3 +90,70 @@ SELECT
     SUM(CASE WHEN duration_valid = 0 THEN 1 ELSE 0 END) AS invalid_duration,
     SUM(CASE WHEN views_valid = 0 THEN 1 ELSE 0 END) AS invalid_views
 FROM netflix_.netflix_dataset.netflix_validated;
+
+------------------------------------------------------------------------------------------
+---Validating the 3 answers 
+---1 What is the avarage duration of movies compared to shows 
+SELECT
+  `_c2` AS content_type,
+  COUNT(*) AS total_count,
+  AVG(CAST(`_c7` AS DOUBLE)) AS avg_duration_minutes,
+  MIN(CAST(`_c7` AS DOUBLE)) AS min_duration,
+  MAX(CAST(`_c7` AS DOUBLE)) AS max_duration
+FROM
+  `netflix_`.`netflix_dataset`.`netflix_dataset_agent`
+WHERE
+  `_c2` IN ('Movie', 'TV Show')
+  AND `_c7` IS NOT NULL
+  AND `_c7` != 'duration_min'
+  AND `_c7` != ''
+GROUP BY
+  `_c2`
+ORDER BY
+  `_c2`;
+
+
+---2 Are ratings improving over time
+SELECT
+  CAST(`_c3` AS INT) AS release_year,
+  COUNT(*) AS content_count,
+  AVG(CAST(`_c6` AS DOUBLE)) AS avg_rating,
+  MIN(CAST(`_c6` AS DOUBLE)) AS min_rating,
+  MAX(CAST(`_c6` AS DOUBLE)) AS max_rating,
+  STDDEV(CAST(`_c6` AS DOUBLE)) AS stddev_rating
+FROM
+  `netflix_`.`netflix_dataset`.`netflix_dataset_agent`
+WHERE
+  `_c3` != 'release_year'
+  AND `_c3` IS NOT NULL
+  AND `_c3` != ''
+  AND `_c6` != 'rating'
+  AND `_c6` IS NOT NULL
+  AND `_c6` != ''
+GROUP BY
+  CAST(`_c3` AS INT)
+ORDER BY
+  release_year;
+
+  ---3 Which genres perfom best in India compared to USA
+  SELECT
+  `_c4` AS genre,
+  `_c5` AS country,
+  AVG(CAST(`_c8` AS DOUBLE)) AS avg_views_millions
+FROM
+  `netflix_`.`netflix_dataset`.`netflix_dataset_agent`
+WHERE
+  `_c5` IN ('India', 'USA')
+  AND `_c5` IS NOT NULL
+  AND `_c4` IS NOT NULL
+  AND `_c4` != 'genre'
+  AND `_c4` != ''
+  AND `_c8` IS NOT NULL
+  AND `_c8` != 'views_millions'
+  AND `_c8` != ''
+GROUP BY
+  `_c4`,
+  `_c5`
+ORDER BY
+  genre,
+  country
